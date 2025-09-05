c22d1a88e4134bdc0008a9dcd01c81175d9fb075
Patr1ck Shaw <133486225+Patr1ck-74@users.noreply.github.com>
2025-09-05 10:12:06 +0800

    Update SiteFooter.vue

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -50,9 +50,9 @@ const { public: configPublic } = useRuntimeConfig();
 const { siteIcp, version } = configPublic;
 
 const linkData = {
-  github: "https://github.com/imsyy/site-status",
-  home: "https://www.imsyy.top",
-  email: "mailto:one@imsyy.top",
+  github: "https://github.com/Patr1ck-74/site-status",
+  home: "https://patr1ck.qzz.io",
+  email: "leslie.en.x@gmail.com",
 };
 </script>
 
